dc9a41d0db86657975e3504fdc715ce06de09be6
Robbie Parks <r.parks15@imperial.ac.uk>
2016-07-04 15:46:05 +0100

    more on Japan

--- Japan/prog/prep_data/Japan_prefecture_monthly_prepare_data_1.0.R
+++ Japan/prog/prep_data/Japan_prefecture_monthly_prepare_data_1.0.R
@@ -1,19 +1,19 @@
 rm(list=ls())
 
 library(dplyr)
-library(foreign)
 
 # function to summarise a year's data..
 monthsummary.JPN  <- function(x=-1) {
     
     rds   <- "datmortjapan20160307"
-    dat <- readRDS(rds)
+    dat <- readRDS(paste0('../../data/',rds))
     
-    dat <- dat[c('deathyear','deathmonth','sex','age5','pref','deaths')]
-    colnames(dat) <- c('year','month','sex','age','pref','deaths')
-    dat.omitted <- dat[is.na(dat$age) | is.na(dat$year) | is.na(dat$month) | dat$pref==sort(unique(dat$pref))[1],]
+    dat <- dat[c('deathyear','deathmonth','sex','age5','pref','pref_IHME','deaths')]
+    colnames(dat) <- c('year','month','sex','age','pref','fips','deaths')
+    dat.omitted <- dat[is.na(dat$age) | is.na(dat$year) | is.na(dat$month) | 
+			dat$pref==sort(unique(dat$pref))[1],]
     dat <- na.omit(dat)
-    dat <- subset(dat,pref!=sort(unique(dat$pref))[1])
+    #dat <- subset(dat,pref!=sort(unique(dat$pref))[1])
     # fix age classes. 999 appearing means something is going wrong
     dat$age <- ifelse (dat$age==0,   0,
     ifelse (dat$age==5,     5,
@@ -37,35 +37,61 @@ monthsummary.JPN  <- function(x=-1) {
     ifelse (dat$age==95,   85,
     ifelse (dat$age==100,  85,
     999)))))))))))))))))))))
-    library(dplyr)
-    dat <- dplyr::summarise(group_by(dat,year,month,sex,age,pref),sum(deaths))
-    colnames(dat)[6] <- c('deaths')
+    
+    # sex into numeric form
+    levels(dat$sex) <- c(2,1)
+
+    dat <- dplyr::summarise(group_by(dat,year,month,sex,age,pref,fips),sum(deaths))
+    colnames(dat)[7] <- c('deaths')
+
+    # create an exhaustive list of location sex age month 
+    # (in this case it should be 47 * 2 * 10 * 12 * 43 = 485040 rows)
+	#pref <- 	unique(dat$pref)
+	fips  <- 	unique(dat$fips)
+	month <- 	c(1:12)
+	year  <- 	c(min(dat$year):max(dat$year))
+	sex   <- 	as.factor(c(1:2))
+	age   <- 	c(0,5,15,25,35,45,55,65,75,85)
+
+	complete.grid <- expand.grid(fips=fips,month=month,year=year,sex=sex,age=age)
+
+    # merge deaths counts with complete grid to ensure there are rows with zero deaths
+    dat.complete <- merge(complete.grid,dat,
+    by=c('fips','year','month','sex','age'),all.x='TRUE')
+
+    # assign missing deaths to have value 0
+    dat.complete$deaths <- ifelse(is.na(dat.complete$deaths)==TRUE,0,dat.complete$deaths)
+
     #dat$month <- month.name[as.numeric(dat$month)]
-    return(dat)
+    return(dat.complete)
 }
 
 dat.appended <- monthsummary.JPN()
 
 # only retain 1980 - 2011 and up to 65 - 74 age group
-dat.appended$age <- as.numeric(as.character(dat.appended$age))
-dat.appended <- dat.appended[dat.appended$year>=1980 & dat.appended$age<75,]
+#dat.appended$age <- as.numeric(as.character(dat.appended$age))
+dat.appended <- dat.appended[dat.appended$year>=1981 & dat.appended$age<75,]
 
 # Add JPN label
 dat.appended$iso3 <- "JPN"
 
 # reorder appended data
-dat.appended <- dat.appended[order(dat.appended$pref,dat.appended$sex,dat.appended$age,dat.appended$year,dat.appended$month),]
-dat.appended <- dat.appended[,c(3,4,1,2,5,7,6)]
-
-# fix sex label
-levels(dat.appended$sex) <- c(2,1)
-dat.appended$sex <- as.integer(as.character(dat.appended$sex))
+dat.appended <- dat.appended[order(dat.appended$fips,dat.appended$sex,dat.appended$age,dat.appended$year,dat.appended$month),]
+#dat.appended <- dat.appended[,c(3,4,1,2,5,7,6)]
 
 # add inferred population data by day
-pop.pref <- readRDS('prefPopulations_infer_by_days')
+pop.pref <- readRDS('../../data/prefPopulations_infer_by_days')
+
+# create lookup for fips and prefecture name
+pref.lookup <- unique(dat.appended[,c('pref', 'fips')])
+pref.lookup <- na.omit(pref.lookup)
+rownames(pref.lookup) <- seq(1:nrow(pref.lookup))
+
+# add fips codes to population file
+pop.pref <- merge(pop.pref,pref.lookup,by='pref',all.x='TRUE')
 
 # merge deaths and population files
-dat.merged <- merge(dat.appended,pop.pref,by=c('sex','age','year','month','pref'))
+dat.merged <- merge(dat.appended,pop.pref,by=c('sex','age','year','month','fips','pref'),all.x='TRUE')
  
 # extract unique table of year and months to generate year.month
 dat.merged.year.month <- unique(dat.merged[,c('year', 'month')])
@@ -77,14 +103,17 @@ dat.merged.year.month$year.month <- seq(nrow(dat.merged.year.month))
 dat.merged <- merge(dat.merged,dat.merged.year.month, by=c('year','month'))
 
 # reorder
-dat.merged <- dat.merged[order(dat.merged$pref,dat.merged$sex,dat.merged$age,dat.merged$year,dat.merged$month),]
+dat.merged <- dat.merged[order(dat.merged$fips,dat.merged$sex,dat.merged$age,dat.merged$year,dat.merged$month),]
 
 # add rates
 dat.merged$rate <- dat.merged$deaths / dat.merged$pop
 dat.merged$rate.adj <- dat.merged$deaths / dat.merged$pop.adj
 
+# fix row names
+rownames(dat.merged) <- seq(1:nrow(dat.merged))
+
 # plot to check rates
 plot(dat.merged$rate,dat.merged$rate.adj)
 
 # output file as RDS
-saveRDS(dat.merged,'datjpn_pref_rates_1980_2011')
+saveRDS(dat.merged,'../../data/datjpn_pref_rates_1980_2011')
---
+++ USA/state/prog/prep_data/US_state_monthly_prepare_data_1.2.R~
@@ -0,0 +1,110 @@
+rm(list=ls())
+
+library(dplyr)
+library(foreign)
+
+# Function to summarise a year's data. x is the year in 2 number form (e.g. 1989 -> 89).
+# y is the number of rows. default (-1) is all rows.
+yearsummary  <- function(x=2000,y=-1) {
+  
+	# load year of deaths
+	dat.name <- paste0("deaths",x,".dta")
+	dat <- read.dta(dat.name)
+
+	# load lookup for fips
+	fips.lookup <- read.csv('fipsMap.csv')
+	dat$fips <- as.numeric(dat$fips)
+
+	# merge files by fips code and keep stateFips info
+	dat.merged <- merge(dat,fips.lookup,by='fips',all.x='TRUE')
+
+	# add agegroup groupings
+  	dat.merged$agegroup <-  
+              	ifelse (dat.merged$age<5,   0,
+                ifelse (dat.merged$age<15,  5,        
+                ifelse (dat.merged$age<25,  15, 
+                ifelse (dat.merged$age<35,  25, 
+                ifelse (dat.merged$age<45,  35, 
+                ifelse (dat.merged$age<55,  45, 
+                ifelse (dat.merged$age<65,  55, 
+                ifelse (dat.merged$age<75,  65, 
+                ifelse (dat.merged$age<85,  75,
+                   	85)))))))))
+
+	# summarise by state,year,month,sex,ageroup
+  	dat.summarised <- summarise(group_by(dat.merged,stateFips,year,monthdth,sex,agegroup),sum(deaths))
+  	names(dat.summarised)[1:6] <- c('fips','year','month','sex','age','deaths')
+	dat.summarised <- na.omit(dat.summarised)
+
+	# create an exhaustive list of location sex age month (in this case it should be 51 * 2 * 10 * 12 = 12240 rows)
+	fips <- 	c(1,2,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,
+			  29,30,31,32,33,34,35,36,37,38,39,40,41,42,44,45,46,47,48,49,50,51,53,54,55,56)
+	month <- 	c(1:12)
+	sex <- 		c(1:2)
+	age <- 		c(0,5,15,25,35,45,55,65,75,85)
+
+	complete.grid <- expand.grid(fips=fips,month=month,sex=sex,age=age)
+	complete.grid$year <- unique(dat.summarised$year)
+
+	# merge deaths counts with complete grid to ensure there are rows with zero deaths
+	dat.summarised.complete <- merge(complete.grid,dat.summarised,by=c('fips','year','month','sex','age'),all.x='TRUE')
+
+	# assign missing deaths to have value 0
+	dat.summarised.complete$deaths <- ifelse(is.na(dat.summarised.complete$deaths)==TRUE,0,dat.summarised.complete$deaths)
+
+  	return(dat.summarised.complete)
+	}
+
+# Function to append all the years desired to be summarised into one file
+appendyears  <- function(x=1980, y=1981) {
+  	years <- x:y
+  	z     <- x
+  	dat   <- data.frame()
+  	while (z %in% years) {
+    		dat <- rbind(dat, yearsummary(z))
+    		print(paste0(z," done"))
+    		z <- z+1
+  	}
+  	return(dat)
+}
+
+# append summarised dataset
+dat.appended <- appendyears(1982,2010)
+
+# reorder appended data
+dat.appended <- dat.appended[order(dat.appended$fips,dat.appended$sex,dat.appended$age,dat.appended$year,dat.appended$month),]
+
+# Filter missing age results and complete results
+#dat.appended.NA <- dat.appended[is.na(dat.appended$age),]
+dat.appended   <- na.omit(dat.appended)
+
+# Add USA label
+dat.appended$iso3 <- "USA"
+
+# add inferred population data by day
+pop.state <- readRDS('statePopulations_infer_by_days')
+pop.state$fips <- as.integer(pop.state$fips)
+
+# merge deaths and population files
+dat.merged <- merge(dat.appended,pop.state,by=c('sex','age','year','month','fips'))
+ 
+# extract unique table of year and months to generate year.month
+#dat.merged.year.month <- unique(dat.merged[,c('year', 'month')])
+#dat.merged.year.month$month <- as.integer(dat.merged.year.month$month)
+#dat.merged.year.month$year.month <- seq(nrow(dat.merged.year.month))
+
+# merge year.month table with population table to create year.month id
+#dat.merged <- merge(dat.merged,dat.merged.year.month, by=c('year','month'))
+
+# reorder
+dat.merged <- dat.merged[order(dat.merged$fips,dat.merged$sex,dat.merged$age,dat.merged$year,dat.merged$month),]
+
+# add rates
+dat.merged$rate <- dat.merged$deaths / dat.merged$pop
+dat.merged$rate.adj <- dat.merged$deaths / dat.merged$pop.adj
+
+# plot to check rates
+plot(dat.merged$rate,dat.merged$rate.adj)
+
+# output file as RDS
+saveRDS(dat.merged,'datus_state_rates_1982_2010')
