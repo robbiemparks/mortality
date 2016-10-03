fbb19a9d5e9ebef2c6cef1549a394a0c6753a129
Robbie Parks <r.parks15@imperial.ac.uk>
2016-10-03 12:59:42 +0100

    made more user-friendly

--- USA/state/prog/00_bash/data_prep.sh
+++ USA/state/prog/00_bash/data_prep.sh
@@ -6,15 +6,21 @@
 # processes monthly death rates by merging death counts with population
 # plots data by age gender state combination to check that it looks OK
 
+clear
+
+declare -a years=(2005 $(seq 2011 2015))
+declare -i start=2012
+declare -i end=2015
+declare -i start2=1982
+declare -i end2=2013
+
 #################################################
 # 1. FORMAT RAW POPULATION FILES
 #################################################
 
-for year in $(seq 2005 2015);
+for year in "${years[@]}"; do
 
-do
-
-echo $year
+echo "formatting raw population files for $year";
 
 Rscript ~/git/mortality/USA/state/prog/pop_format/pop_format.R $year
 
@@ -24,11 +30,9 @@ done;
 # 2. COMPARE OLD AND NEW POP FILES
 #################################################
 
-for year in $(seq 2005 2011);
+for year in "${years[@]}"; do
 
-do
-
-echo $year
+echo "comparing old and new population files for $year";
 
 Rscript ~/git/mortality/USA/state/prog/pop_format/pop_old_new_compare.R $year
 
@@ -38,11 +42,15 @@ done;
 # 3. ADD NEW YEARS TO POPULATION FILES
 #################################################
 
-Rscript ~/git/mortality/USA/state/prog/pop_format/pop_add_new_years.R 2012 2015
+echo "adding years $start - $end to population files";
+
+Rscript ~/git/mortality/USA/state/prog/pop_format/pop_add_new_years.R $start $end
 
 #################################################
 # 4. RUN POPULATION INFER BY DAY
 #################################################
+
+echo "inferring monthly populations from yearly figures for years $start2 - $end2";
 
 Rscript ~/git/mortality/USA/state/prog/pop_us_infer/pop_us_infer_state_days.R
 
@@ -50,10 +58,14 @@ Rscript ~/git/mortality/USA/state/prog/pop_us_infer/pop_us_infer_state_days.R
 # 5. PROCESS DATA
 #################################################
 
-Rscript ~/git/mortality/USA/state/prog/prep_data/US_state_monthly_prepare_data.R 1982 2013
+echo "preparing monthly death rates for years $start2 - $end2";
+
+Rscript ~/git/mortality/USA/state/prog/prep_data/US_state_monthly_prepare_data.R $start2 $end2
 
 #################################################
 # 6. EXPLORE DATA
 #################################################
 
-Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore.R 1982 2013
+echo "exploring death rates for years $start2 - $end2";
+
+Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore.R $start2 $end2
---
+++ USA/state/prog/00_bash/plotting_results.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this script
+# plots results of posterior of model
+
+clear
+
+declare -a ages=(0 5 15 25 35 45 55 65 75 85)
+declare -i model=2
+declare -i start=1982
+declare -i end=2013
+declare country="USA"
+
+#################################################
+# 1. PLOT 1
+#################################################
+
+# PLOT SOMETHING
+
+#################################################
+# 2. PLOT 2
+#################################################
+
+# PLOT SOMETHING ELSE
+
+#################################################
+# 3. PLOT 3
+#################################################
+
+# PLOT SOMETHING ELSE AGAIN
