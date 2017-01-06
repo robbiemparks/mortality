066613b03397739b9c505c5bb300b081220d9654
Robbie Parks <rmiparks@Robbie-Parkss-Big-MacBook-Pro.local>
2017-01-06 10:22:43 +0000

    initial updates to Japan data

--- Japan/pref/prog/data_explore/data_explore.R
+++ Japan/pref/prog/data_explore/data_explore.R
@@ -2,18 +2,22 @@ rm(list=ls())
 
 library(plyr)
 library(ggplot2)
+library(zoo)
 
 # break down the arguments from Rscript
 args <- commandArgs(trailingOnly=TRUE)
 
 # gender state and age lookup
-gender.lookup <- c('male','female')
-age.lookup <- unique(dat$age)
+gender.lookup <- c('Men','Women')
+
+#age.new.lookup <- data.frame(age5=c(0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100),
+#                            age.new=c(0,5,5,15,15,25,25,35,35,45,45,55,55,65,65,75,80,85,85,95,95))
+
+# NATIONAL
 
 ############################
 # organise popualaton data
 ############################
-
 
 # load population data
 filename <- paste0('../../data/population/original/datpopjapan20160307')
@@ -22,11 +26,27 @@ dat.pop <- readRDS(filename)
 # only keep national data
 dat.pop.nat <- subset(dat.pop,pref=='0')
 
-# create new column which will group all of the 80+ ages to eventually subtract from 75+ age group
-dat.pop.nat$age.group <- ifelse(dat.pop.nat$age<=75,dat.pop.nat$age,80)
+# rename columns
+dat.pop.nat <- rename(dat.pop.nat,c('deathyear'='year'))
 
-# summarise by new age groupings to be able to compare 80+ with 75+
-dat.pop.nat.2 <- ddply(dat.pop.nat,.(pref,age.group,sex,deathyear),summarize,population=sum(population))
+
+# create grid to attach population values to and interpolate missing values
+#months <- 	c(1:12)
+years  <-    c(min(dat.pop.nat$year):max(dat.pop.nat$year))
+sexes  <- 	 c('Male','Female')
+ages   <-    c(0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100)
+
+#complete.grid <- expand.grid(year=years,month=months,sex=sexes,age=ages)
+complete.grid <- expand.grid(year=years,sex=sexes,age=ages)
+
+# merge population with complete grid to highlight missing years
+dat.pop.nat.complete <- merge(complete.grid,dat.pop.nat,by=c('year','sex','age'),all.x='TRUE')
+
+# reorder complete file
+dat.pop.nat.complete <- dat.pop.nat.complete[order(dat.pop.nat.complete$age,dat.pop.nat.complete$sex,dat.pop.nat.complete$year),]
+
+# interpolate using zoo and ddply package LIKE BELOW BUT NEEDS TO BE CHANGED
+dat.pop.nat.complete <- ddply(dat.pop.nat.complete,.(sex,age),function(z)coef(summary(lm(percent.change ~ year.centre, data=z))))
 
 ############################
 # organise mortality data
@@ -36,10 +56,8 @@ dat.pop.nat.2 <- ddply(dat.pop.nat,.(pref,age.group,sex,deathyear),summarize,pop
 filename <- paste0('../../data/mortality/original/datmortjapan20160307')
 dat.mort <- readRDS(filename)
 
-# summarise mortality data
-
-
-# NATIONAL
+# summarise mortality data nationally
+dat.mort.nat <- ddply(dat.mort,.(age5,sex,deathyear),summarize,deaths=sum(deaths))
 
 # OUTPUT PLOTS
 
@@ -47,16 +65,16 @@ dat.mort <- readRDS(filename)
 file.loc <- paste0('../../output/data_explore/national/')
 ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
 
+# plot national deaths by age group over time
+pdf(paste0(file.loc,'japan_nat_deaths_1969_2011','.pdf'),height=0,width=0,paper='a4r')
+ggplot(data=dat.mort.nat,aes(x=deathyear,y=deaths)) +
+geom_line(aes(color=as.factor(age5))) +
+facet_wrap(~sex)
+dev.off()
+
 # plot national population by age group over time
 pdf(paste0(file.loc,'japan_nat_pop_1975_2013','.pdf'),height=0,width=0,paper='a4r')
 ggplot(data=dat.pop.nat,aes(x=deathyear,y=population)) +
 geom_line(aes(color=as.factor(age))) +
 facet_wrap(~sex)
 dev.off()
-
-pdf(paste0(file.loc,'japan_nat_pop_adj_1975_2013','.pdf'),height=0,width=0,paper='a4r')
-ggplot(data=dat.pop.nat.2,aes(x=deathyear,y=population)) +
-geom_line(aes(color=as.factor(age.group))) +
-facet_wrap(~sex)
-dev.off()
-
