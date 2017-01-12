ec70534f9c3f23c519b5d4094fa5e811a408ab69
Robbie Parks <rmiparks@dyn1182-213.wlan.ic.ac.uk>
2017-01-12 14:40:27 +0000

    added updates

--- Japan/pref/prog/data_explore/data_explore.R
+++ Japan/pref/prog/data_explore/data_explore.R
@@ -10,7 +10,7 @@ args <- commandArgs(trailingOnly=TRUE)
 # gender state and age lookup
 gender.lookup <- c('Men','Women')
 
-# NATIONAL
+# NATIONAL YEARLY
 
 ############################
 # organise popualaton data
@@ -34,7 +34,6 @@ years  <-    c(min(dat.pop.nat$year):max(dat.pop.nat$year))
 sexes <- c(1:2)
 ages   <-    c(0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100)
 
-#complete.grid <- expand.grid(year=years,month=months,sex=sexes,age=ages)
 complete.grid <- expand.grid(year=years,sex=sexes,age=ages)
 
 # merge population with complete grid to highlight missing years
@@ -58,10 +57,33 @@ dat.pop.nat.complete <- ddply(dat.pop.nat.complete,.(sex,age),function(z) (na.ap
 filename <- paste0('../../data/mortality/original/datmortjapan20160307')
 dat.mort <- readRDS(filename)
 
+# rename columns
+dat.mort <- rename(dat.mort,c('deathyear'='year','age5'='age'))
+
+# rename sexes
+dat.mort$sex <- as.numeric(as.character(revalue(dat.mort$sex, c("Male"="1", "Female"="2"))))
+
 # summarise mortality data nationally
-dat.mort.nat <- ddply(dat.mort,.(age5,sex,deathyear),summarize,deaths=sum(deaths))
+dat.mort.nat <- ddply(dat.mort,.(age,sex,year),summarize,deaths=sum(deaths))
+
+# remove unknown records
+dat.mort.nat <- na.omit(dat.mort.nat)
+
+############################
+# merge population and mortality data
+############################
+
+dat.merged <- merge(dat.mort.nat,dat.pop.nat.complete,by=c('year','age','sex'),all.x=1)
+
+# only keep 1980-2010
+dat.merged <- subset(dat.merged,year %in% c(1980:2010))
+
+# calculate death rates
+dat.merged$rate <- 100000*with(dat.merged,deaths/population)
 
 # OUTPUT PLOTS
+
+# YEARLY
 
 # create directory for output
 file.loc <- paste0('../../output/data_explore/national/')
@@ -70,7 +92,7 @@ ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
 # plot national deaths by age group over time
 pdf(paste0(file.loc,'japan_nat_deaths_1969_2011','.pdf'),height=0,width=0,paper='a4r')
 ggplot(data=dat.mort.nat,aes(x=year,y=deaths)) +
-geom_line(aes(color=as.factor(age5))) +
+geom_line(aes(color=as.factor(age))) +
 facet_wrap(~sex)
 dev.off()
 
@@ -84,5 +106,12 @@ dev.off()
 pdf(paste0(file.loc,'japan_nat_pop_1975_2013_interpolated','.pdf'),height=0,width=0,paper='a4r')
 ggplot() +
 geom_line(dat=na.omit(dat.pop.nat.complete),aes(x=year,y=population,color=as.factor(age))) +
+facet_wrap(~sex)
+dev.off()
+
+# plot national death rates by age group over time
+pdf(paste0(file.loc,'japan_nat_log_deathrates_1980_2010','.pdf'),height=0,width=0,paper='a4r')
+ggplot(data=dat.merged,aes(x=year,y=log(rate))) +
+geom_line(aes(color=as.factor(age))) +
 facet_wrap(~sex)
 dev.off()
