dddbcc08eb696c7c849a8f681c02dbd9ee945609
Robbie Parks <rmiparks@Robbie-Parkss-Big-MacBook-Pro.local>
2017-01-26 19:13:29 +0000

    added stuff

--- USA/state/prog/seasonality_index/seasonality_index.R
+++ USA/state/prog/seasonality_index/seasonality_index.R
@@ -709,7 +709,7 @@ ggplot() +
 geom_point(data=subset(dat.mort.climate.fixed, sex==1|2),aes(shape=as.factor(sex),x=end.value.climate,y=end.value.mort/100,color=as.factor(climate_region)),size=2) +
 scale_shape_manual(values=c(16,17),labels=c('Men','Women'),guide = guide_legend(title = 'Sex:')) +
 scale_x_continuous(name=expression(paste("Temperature difference (",degree,"C) between maximum and minimum months"))) +
-scale_y_continuous(name=paste0('Seasonal excess mortality in ',year.end.2),labels=percent) +
+scale_y_continuous(name=paste0('Seasonal percent excess mortality in ',year.end.2),labels=percent) +
 #geom_hline(linetype=2, yintercept = seq(-1,1,0.1), alpha=0.2) +
 #geom_vline(linetype=2, xintercept = seq(-100,100,10), alpha=0.2) +
 geom_vline(xintercept=0,linetype=2,alpha=0.4) +
