d45de632069cdafc536d973d85e6a038a84891de
rmp15 <r.parks15@imperial.ac.uk>
2017-03-09 22:03:01 +0000

    Attempt at Rossby wave calculations

---
+++ USA/state/prog/rossby/rossby.R
@@ -0,0 +1,9 @@
+rm(list=ls())
+
+# break down the arguments from Rscript
+args <- commandArgs(trailingOnly=TRUE)
+year.start <- as.numeric(args[1])
+year.end <- as.numeric(args[2])
+
+# add sourced data
+source('../../data/objects/objects.R')
