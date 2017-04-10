6c74e4dfa41df8f910b8df1c6e9142043167f43c
Robbie M Parks <rmp15@wrfproc1-10.sp.ph.ic.ac.uk>
2017-04-10 05:06:45 +0100

    added stuff

--- USA/state/prog/00_bash/inla_model_climate.sh
+++ USA/state/prog/00_bash/inla_model_climate.sh
@@ -7,8 +7,8 @@
 
 clear
 
-declare -a ages=(85)
-declare -a sexes=(1)
+declare -a ages=(75)
+declare -a sexes=(1 2)
 declare -a sexstrings=('male' 'female')
 declare -a models=(11)
 declare -i start=1982
@@ -36,10 +36,48 @@ for model in "${models[@]}"; do
 :
 # runs model
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate.R $age $sex $start $end $model 0 $dname $metric
-Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R $age $sex $start $end $model 0 $dname $metric &
+#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R $age $sex $start $end $model 0 $dname $metric &
 
 done; done; done; done;
 
+(
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 65 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 65 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 55 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 55 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 45 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 45 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 35 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 35 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 5  1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 5  2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 0  1 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 0  2 1982 2013 11 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 65 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 65 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 55 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 55 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 45 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 45 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 35 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 35 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 5  1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 5  2 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 0  1 1982 2013 10 0 't2m' 'meanc' ;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 0  2 1982 2013 10 0 't2m' 'meanc' ;
+) &
 
 #################################################
 # 2. COMBINE RESULTS
