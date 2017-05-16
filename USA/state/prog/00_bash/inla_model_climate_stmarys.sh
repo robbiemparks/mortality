cbbeedbf720f4aa665d89b278e4f5743cc4bdec9
rmp15 <r.parks15@imperial.ac.uk>
2017-05-16 17:52:45 +0100

    added stuff

--- USA/state/prog/00_bash/inla_model_climate_stmarys.sh
+++ USA/state/prog/00_bash/inla_model_climate_stmarys.sh
@@ -25,11 +25,11 @@ declare metric="number_of_min_3_day_above_+5_jumpupwaves"
 
 (
 
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 85 1 $start $end 10 0 $dname $metric $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 85 2 $start $end 10 0 $dname $metric $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 1 $start $end 10 0 $dname $metric $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 2 $start $end 10 0 $dname $metric $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 65 1 $start $end 10 0 $dname $metric $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 85 1 $start $end 10 0 $dname $metric $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 85 2 $start $end 10 0 $dname $metric $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 1 $start $end 10 0 $dname $metric $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 75 2 $start $end 10 0 $dname $metric $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 65 1 $start $end 10 0 $dname $metric $start2 $end2;
 
 ) &
 
