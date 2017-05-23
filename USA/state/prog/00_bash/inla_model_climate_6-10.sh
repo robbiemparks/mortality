cef510bcd1bd88961eb9f9e641cfed60ddd7c907
rmp15 <r.parks15@imperial.ac.uk>
2017-05-23 10:19:48 +0100

    added bash

--- USA/state/prog/00_bash/inla_model_climate_6-10.sh
+++ USA/state/prog/00_bash/inla_model_climate_6-10.sh
@@ -42,10 +42,10 @@ declare metric="90percc3"
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 35 2 $start $end 10 0 $dname $metric $start2 $end2;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 35 1 $start $end 10 0 $dname $metric $start2 $end2;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 2 $start $end 10 0 $dname $metric $start2 $end2;
-Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 1 $start $end 10 0 $dname $metric $start2 $end2;
-Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 2 $start $end 10 0 $dname $metric $start2 $end2;
+#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 25 1 $start $end 10 0 $dname $metric $start2 $end2;
+#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 2 $start $end 10 0 $dname $metric $start2 $end2;
 
-Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 1 $start $end 10 0 $dname $metric $start2 $end2;
+#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R 15 1 $start $end 10 0 $dname $metric $start2 $end2;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R  5 2 $start $end 10 0 $dname $metric $start2 $end2;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R  5 1 $start $end 10 0 $dname $metric $start2 $end2;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_fast.R  0 2 $start $end 10 0 $dname $metric $start2 $end2;
