8c5ad1e4710e2b3862e4670098569cf5ad00c60c
rmp15 <r.parks15@imperial.ac.uk>
2017-11-08 23:50:57 +0000

    added stuff

--- USA/state/prog/00_bash/inla/inla_model_climate_3var_stmarys.sh
+++ USA/state/prog/00_bash/inla/inla_model_climate_3var_stmarys.sh
@@ -27,33 +27,33 @@ declare metric3="number_of_min_5_day_below_+5_jumpdownwaves_2"
 # 1. RUN AGE-SEPARATED MODEL
 #################################################
 
-#(
+(
 
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 85 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 85 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 75 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 75 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 65 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 85 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 85 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 75 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 75 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 65 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
 
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 65 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 55 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 55 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 45 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 45 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 65 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 55 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 55 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 45 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 45 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
 
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 35 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 35 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 25 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 25 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 15 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 35 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 35 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 25 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 25 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 15 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
 
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 15 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  5 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  5 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  0 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  0 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R 15 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  5 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  5 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  0 2 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_3var_fast.R  0 1 $start $end 10 0 $dname $metric1 $metric2 $metric3 $start2 $end2;
 
-#) &
+) &
 
 #################################################
 # 2. COMBINE RESULTS
@@ -63,6 +63,6 @@ for model in "${models[@]}"; do
 
 echo "combining results into one file from INLA model $model years $start - $end";
 
-Rscript ~/git/mortality/USA/state/prog/bind_posterior/bind_posterior_climate_3var.R $start $end $country $model $dname $metric1 $metric2 $metric3
+#Rscript ~/git/mortality/USA/state/prog/bind_posterior/bind_posterior_climate_3var.R $start $end $country $model $dname $metric1 $metric2 $metric3
 
 done;
