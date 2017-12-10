f1deb3d058c7fe5b2fa10925025d05fa86eb142d
rmp15 <r.parks15@imperial.ac.uk>
2017-12-10 15:57:23 +0000

    Subnational population in progress

--- USA/state/prog/00_bash/data_prep/data_explore_cod.sh
+++ USA/state/prog/00_bash/data_prep/data_explore_cod.sh
@@ -6,7 +6,7 @@
 
 clear
 
-declare -i start=1982
+declare -i start=1980
 declare -i end=2013
 declare -a states=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23"
 "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48"
@@ -28,7 +28,7 @@ echo "plotting population against time by county";
 echo "plotting raw death rates by state for years $start - $end";
 
 # runs code
-Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore.R $start $end
+Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_cod.R $start $end
 
 #################################################
 # 3. FIPS CHECK OVER TIME
