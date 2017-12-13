b40c7c728a24dbd2acbe89bbbcf61bf20b81388f
rmp15 <r.parks15@imperial.ac.uk>
2017-12-13 08:04:39 +0000

    updated cod mortality models

--- USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_7-10.sh
+++ USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_7-10.sh
@@ -26,7 +26,7 @@ declare cod="External"
 
 (
 
-#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R 85 2 $start $end 10 0 $dname $metric $start2 $end2 $cod;
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R 85 2 $start $end 10 0 $dname $metric $start2 $end2 $cod;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R 85 1 $start $end 10 0 $dname $metric $start2 $end2 $cod;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R 75 2 $start $end 10 0 $dname $metric $start2 $end2 $cod;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R 75 1 $start $end 10 0 $dname $metric $start2 $end2 $cod;
