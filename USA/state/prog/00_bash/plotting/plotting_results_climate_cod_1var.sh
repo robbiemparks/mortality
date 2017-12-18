f1177ed7489000aae54c4c6778c0899ae6efce84
rmp15 <r.parks15@imperial.ac.uk>
2017-12-18 22:46:58 +0000

    updated cod mortality models with 3 variables

--- USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
+++ USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
@@ -8,7 +8,7 @@ cd ~/git/mortality/USA/state/prog/00_bash/
 clear
 
 declare -i start=1980
-declare -i end=1989
+declare -i end=2013
 declare -a models=(10)
 declare country="USA"
 declare -a dnames=("t2m")
