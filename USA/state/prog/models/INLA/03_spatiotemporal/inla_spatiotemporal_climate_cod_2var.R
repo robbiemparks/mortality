2ff16f900b4114f5bef76a50d5ada4c1f1e395e7
rmp15 <r.parks15@imperial.ac.uk>
2017-12-27 14:50:26 +0000

    updated cod mortality models with 3 variables

--- USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_2var.R
+++ USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_2var.R
@@ -13,14 +13,13 @@ cluster.arg <- as.numeric(args[6])
 dname.arg <- as.character(args[7])
 metric1.arg <- as.character(args[8])
 metric2.arg <- as.character(args[9])
-metric3.arg <- as.character(args[10])
-year.start.analysis.arg <- as.numeric(args[11])
-year.end.analysis.arg <- as.numeric(args[12])
-cod.arg <- as.character(args[13])
+year.start.analysis.arg <- as.numeric(args[10])
+year.end.analysis.arg <- as.numeric(args[11])
+cod.arg <- as.character(args[12])
 
 # age.arg = 65 ; sex.arg = 1 ; year.start.arg = 1980 ; year.end.arg = 2013 ; type.arg = 10 ;
-# cluster.arg = 0 ; dname.arg = 't2m' ; metric1.arg = 'meanc3' ; metric2.arg = '' ; metric3.arg = '';
-# year.start.analysis.arg = 1980 ; # year.end.analysis.arg = 1989 ; cod.arg = 'Cancer'
+#cluster.arg = 0 ; dname.arg = 't2m' ; metric1.arg = 'meanc3' ; metric2.arg = 'number_of_days_above_nonnormal_90_2' ;
+#year.start.analysis.arg = 1980 ; # year.end.analysis.arg = 1989 ; cod.arg = 'Cardiopulmonary'
 
 # types character for file strings
 types <- c('1','1a','2','2a','3','3a','4','1b','1c','1d','1e','1f','1de','1ef','1g','0','minus1')
@@ -34,10 +33,10 @@ years <- year.start.arg:year.end.arg
 require(mailR)
 
 # combine three metrics in alphabetical order in a single string
-metric.arg = paste(sort(c(metric1.arg,metric2.arg,metric3.arg)),collapse='_')
+metric.arg = paste(sort(c(metric1.arg,metric2.arg)),collapse='_')
 
 # create files for output
-ifelse(!dir.exists(paste0('~/data/mortality/US/state/climate_effects/3var/',dname.arg,'/',metric.arg,'/non_pw/type_',type.selected,'/age_groups')), dir.create(paste0('~/data/mortality/US/state/climate_effects/',dname.arg,'/',metric.arg,'/non_pw/type_',type.selected,'/age_groups'),recursive=TRUE), FALSE)
+ifelse(!dir.exists(paste0('~/data/mortality/US/state/climate_effects/2var/',dname.arg,'/',metric.arg,'/non_pw/type_',type.selected,'/age_groups')), dir.create(paste0('~/data/mortality/US/state/climate_effects/',dname.arg,'/',metric.arg,'/non_pw/type_',type.selected,'/age_groups'),recursive=TRUE), FALSE)
 
 # load data and filter results
 if(cod.arg!='AllCause'){
@@ -75,14 +74,11 @@ dat.inla.load <- merge(dat.inla.load,dat.region,by.x=('fips'),by.y=('STATE_FIPS'
 # load climate data NEED TO GENERALISE
 file.loc1 <- paste0('~/git/climate/countries/USA/output/metrics_development/',dname.arg,'/',metric1.arg,'_',dname.arg,'/')
 file.loc2 <- paste0('~/git/climate/countries/USA/output/metrics_development/',dname.arg,'/',metric2.arg,'_',dname.arg,'/')
-file.loc3 <- paste0('~/git/climate/countries/USA/output/metrics_development/',dname.arg,'/',metric3.arg,'_',dname.arg,'/')
 dat.climate1 <- readRDS(paste0(file.loc1,'state_weighted_summary_',metric1.arg,'_',dname.arg,'_1979_2015.rds'))
 dat.climate2 <- readRDS(paste0(file.loc2,'state_weighted_summary_',metric2.arg,'_',dname.arg,'_1979_2015.rds'))
-dat.climate3 <- readRDS(paste0(file.loc3,'state_weighted_summary_',metric3.arg,'_',dname.arg,'_1979_2015.rds'))
 
 dat.climate1$state.fips <- as.numeric(as.character(dat.climate1$state.fips))
 dat.climate2$state.fips <- as.numeric(as.character(dat.climate2$state.fips))
-dat.climate3$state.fips <- as.numeric(as.character(dat.climate3$state.fips))
 
 # leap year test
 is.leapyear=function(year){
@@ -92,21 +88,18 @@ is.leapyear=function(year){
 # add leap year if not already there
 dat.climate1$leap <- as.integer(is.leapyear(dat.climate1$year))
 dat.climate2$leap <- as.integer(is.leapyear(dat.climate2$year))
-dat.climate3$leap <- as.integer(is.leapyear(dat.climate3$year))
 
 # merge mortality and climate data and reorder
 dat.merged <- merge(dat.inla.load,dat.climate1,by.x=c('sex','age','year','month','fips','leap'),by.y=c('sex','age','year','month','state.fips','leap'),all.x=TRUE)
 dat.merged <- merge(dat.merged,dat.climate2,by.x=c('sex','age','year','month','fips','leap'),by.y=c('sex','age','year','month','state.fips','leap'),all.x=TRUE)
-dat.merged <- merge(dat.merged,dat.climate3,by.x=c('sex','age','year','month','fips','leap'),by.y=c('sex','age','year','month','state.fips','leap'),all.x=TRUE)
 dat.merged <- dat.merged[order(dat.merged$climate_region,dat.merged$fips,dat.merged$sex,dat.merged$age,dat.merged$year,dat.merged$month),]
 
 # rename rows and remove unnecessary columns
 rownames(dat.merged) <- 1:nrow(dat.merged)
 
 # generalise climate variable names in alphabetical order
-names(dat.merged)[21]='variable1'
-names(dat.merged)[22]='variable2'
-names(dat.merged)[23]='variable3'
+names(dat.merged)[ncol(dat.merged)-1]='variable1'
+names(dat.merged)[ncol(dat.merged)]='variable2'
 
 # create lookup table for climate regions
 regions.lookup <- data.frame(climate_region=sort(unique(dat.merged$climate_region)))
@@ -130,8 +123,8 @@ USA.adj <- "../../output/adj_matrix_create/USA.graph.edit"
 library(INLA)
 
 # load inla function
-source('../models/INLA/03_spatiotemporal/inla_functions_cod_3var.R')
+source('../models/INLA/03_spatiotemporal/inla_functions_cod_2var.R')
 
 # input arguments into function to perform inference
-mapply(inla.function.climate.3var,age.sel=age.arg,sex.sel=sex.arg,year.start=year.start.analysis.arg,
+mapply(inla.function.climate.2var,age.sel=age.arg,sex.sel=sex.arg,year.start=year.start.analysis.arg,
 year.end=year.end.analysis.arg,type=type.arg,cluster=cluster.arg)
