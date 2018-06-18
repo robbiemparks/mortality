340db7bfcae6d5848c783a377fbf981570160a6b
rmp15 <r.parks15@imperial.ac.uk>
2018-06-18 10:22:52 +0100

    Edited bash file to describe what's going on a bit more for eLife revisions

--- USA/state/prog/00_bash/wavelet/wavelet_paper_cod.sh
+++ USA/state/prog/00_bash/wavelet/wavelet_paper_cod.sh
@@ -106,7 +106,7 @@ echo "plotting COM analysis for $country for $cod, years $start - $end";
 #Rscript ~/git/mortality/USA/state/prog/com/com_data_process_cod.R $start $end $cod
 
 # plots (figure 3, figure 5, appendix figure 2)
-#Rscript ~/git/mortality/USA/state/prog/com/com_plot_cod.R $start $end $cod
+Rscript ~/git/mortality/USA/state/prog/com/com_plot_cod.R $start $end $cod
 
 done;
 
