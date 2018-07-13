dec0945fdec8a5a781d5b8d5031f2627e840da2c
rmp15 <r.parks15@imperial.ac.uk>
2018-07-13 17:53:40 +0100

    Fixed injury paper bash script

--- USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
+++ USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
@@ -14,7 +14,7 @@ declare country="USA"
 declare -a dnames=("t2m")
 declare -a metrics=('meanc3')
 #declare -a cods=("External" "Unintentional" "Unintentional_wo_drowning" "Intentional" "Transport_accidents" "Intentional_self-harm" "Accidental_falls" "Accidental_drowning_and_submersion")
-declare -a cods=("Accidental_drowning_and_submersion")
+declare -a cods=("Assault")
 #declare -a cods=("AllCause" "Cancer" "Cardiopulmonary" "External" "Other" "Unintentional" "Intentional")
 
 declare -i contig=1
@@ -31,7 +31,7 @@ for metric in "${metrics[@]}"; do
 
 for model in "${models[@]}"; do
 
-#Rscript ~/git/mortality/USA/state/prog/bind_posterior/bind_posterior_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig
+Rscript ~/git/mortality/USA/state/prog/bind_posterior/bind_posterior_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig
 Rscript ~/git/mortality/USA/state/prog/mapping_posterior/mapping_posterior_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig
 
 done; done; done; done;
