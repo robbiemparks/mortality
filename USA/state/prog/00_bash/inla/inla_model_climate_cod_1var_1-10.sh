8d227f44470b74205e0009b6f29f60aedb1d0e15
rmp15 <r.parks15@imperial.ac.uk>
2018-08-21 22:59:19 +0100

    updates to wavelet coding

--- USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_1-10.sh
+++ USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_1-10.sh
@@ -107,6 +107,8 @@ declare cod="Accidental_drowning_and_submersion"
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 0  1 $start $end  10 0 $dname $metric $start2 $end2 $cod $fast $contig;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 0  2 $start $end  10 0 $dname $metric $start2 $end2 $cod $fast $contig;
 
+Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 45 2 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig;
+
 declare cod="Assault"
 
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 15 1 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig;
