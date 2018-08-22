a636b6217b97c0f8ef9b5bc4f062fa3bdcea3cf4
rmp15 <rparks15@imperial.ac.uk>
2018-08-22 12:57:54 +0100

    updates to wavelet coding

--- USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_7-10.sh
+++ USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_7-10.sh
@@ -79,7 +79,7 @@ declare cod="Accidental_falls"
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 25 1 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig;
 #Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 15 2 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig;
 
-Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 0  1 $start $end  10 0 $dname $metric $start2 $end2 $cod $fast $contig;
+#Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 0  1 $start $end  10 0 $dname $metric $start2 $end2 $cod $fast $contig;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod.R 0  2 $start $end  10 0 $dname $metric $start2 $end2 $cod $fast $contig;
 
 declare cod="Other_external_causes_of_injury"
