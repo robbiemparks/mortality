4df9a97fd025376dfc190bbde307be95205561ff
rmp15 <r.parks15@imperial.ac.uk>
2018-09-18 20:11:27 +0100

    updates to wavelet coding

--- USA/state/prog/00_bash/additional_deaths/additional_deaths_no_draws_cod_1var.sh
+++ USA/state/prog/00_bash/additional_deaths/additional_deaths_no_draws_cod_1var.sh
@@ -15,7 +15,7 @@ declare metric="meanc3"
 declare -a cods=("Unintentional")
 declare -i fast=1
 declare -i contig=1
-declare -a draws=(1000) #5000) also 5000 when running fully
+declare -a draws=(5000) #5000) also 5000 when running fully
 
 #################################################
 # 1. MAKE DRAWS
@@ -28,13 +28,12 @@ cd ~/git/mortality/USA/state/prog/00_bash/
 
 #declare cod="Transport_accidents"
 
-for cod in "${cods[@]}"; do
 for model in "${models[@]}"; do
 
 echo "combining results into one file from INLA model $model years $start - $end";
 
-Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig;
+Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_cod_1var.R $start $end $country $model $dname $metric $contig $draws;
 
-done; done; done;
+done;
 
 ) &
