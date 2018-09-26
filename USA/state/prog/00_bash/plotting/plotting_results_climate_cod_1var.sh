bf6fcf855b4ac0d965f7b0dcaa6486f029421a93
rmp15 <r.parks15@imperial.ac.uk>
2018-09-26 15:05:50 +0100

    updates to wavelet coding

--- USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
+++ USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
@@ -9,11 +9,11 @@ clear
 
 declare -i start=1980
 declare -i end=2016
-declare -a models=(10)
+declare -a models=(21)
 declare country="USA"
 declare -a dnames=("t2m")
 declare -a metrics=('meanc3')
-declare -a cods=("External" "Unintentional" "Unintentional_wo_drowning" "Intentional" "Transport_accidents" "Intentional_self-harm" "Accidental_falls" "Accidental_drowning_and_submersion" "Assault" "Other_external_causes_of_injury")
+declare -a cods=("External")# "Unintentional" "Unintentional_wo_drowning" "Intentional" "Transport_accidents" "Intentional_self-harm" "Accidental_falls" "Accidental_drowning_and_submersion" "Assault" "Other_external_causes_of_injury")
 #declare -a cods=("AllCause" "Cancer" "Cardiopulmonary" "External" "Other" "Unintentional" "Intentional")
 #declare -a cods=("Accidental_drowning_and_submersion")
 declare -i contig=1
