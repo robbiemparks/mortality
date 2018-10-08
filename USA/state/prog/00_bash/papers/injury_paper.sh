a1ea6835a77413a3fdc41f2bf299e831a0b26c94
rmp15 <r.parks15@imperial.ac.uk>
2018-10-08 16:38:46 +0100

    updates to wavelet coding

--- USA/state/prog/00_bash/papers/injury_paper.sh
+++ USA/state/prog/00_bash/papers/injury_paper.sh
@@ -9,6 +9,17 @@ clear
 declare -i start=1980
 declare -i end=2016
 
+declare -a sexstrings=('male' 'female')
+declare -a model=(10)
+declare -i start=1980
+declare -i end=2016
+declare country="USA"
+declare dname="t2m"
+declare metric="meanc3"
+declare -i fast=1
+declare -i contig=1
+declare -a draws=(5000)
+
 # to correct directory
 cd ~/git/mortality/USA/state/prog/00_bash/
 
@@ -25,8 +36,6 @@ Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_injuries_ons_su
 # Figure 3
 #################################################
 
-declare dname='t2m'
-declare metric='meanc3'
 
 echo "plotting figure 3 injury paper $start - $end";
 
@@ -34,6 +43,7 @@ echo "plotting figure 3 injury paper $start - $end";
 Rscript ~/git/climate/countries/USA/prog/06_plots/plots_against_time.R $start $end $dname $metric
 
 #################################################
-# Figure 4
+# Figure 4 and supplementary figure 1
 #################################################
 
+Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R $start $end $country $model $dname $metric $contig $draws
