44417f3addc039f7be3ff5484205c735a3032075
rmp15 <r.parks15@imperial.ac.uk>
2019-02-05 17:58:47 +0000

    updates to wavelet coding

--- USA/state/prog/data_explore/tree_structure_cods.R
+++ USA/state/prog/data_explore/tree_structure_cods.R
@@ -8,8 +8,17 @@ library(scales)
 file.loc <- paste0('../../output/data_explore_cod/')
 ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
 
+
+
+# plot the tree diagram
 pdf(paste0(file.loc,'tree_structure_cods.pdf'),paper='a4r',height=0,width=0)
-
-
+    # code for tree diagram here
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),text = element_text(size = 15),
+    axis.ticks.x=element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
 dev.off()
 
