05e66047589b35555cbabcca18a5b0ba33564fd4
rmp15 <r.parks15@imperial.ac.uk>
2019-02-05 18:56:55 +0000

    updates to wavelet coding

--- USA/state/prog/data_explore/tree_structure_cods.R
+++ USA/state/prog/data_explore/tree_structure_cods.R
@@ -3,23 +3,25 @@ rm(list=ls())
 library(ggplot2)
 library(RColorBrewer)
 library(scales)
-# library(ggtree)
+library(ggdendro)
 
 # create directories for output
 file.loc <- paste0('../../output/data_explore_cod/')
 ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
 
-
+# test
+hc = hclust(dist(USArrests),'ave')
 
 # plot the tree diagram
 pdf(paste0(file.loc,'tree_structure_cods.pdf'),paper='a4r',height=0,width=0)
     # code for tree diagram here
+    ggdendrogram(hc,rorate=FALSE,size=2) +
     theme_bw() +
-    theme(panel.grid.major = element_blank(),text = element_text(size = 15),
-    axis.ticks.x=element_blank(),
-    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
-    legend.position = 'bottom',legend.justification='center',
-    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+    # theme(panel.grid.major = element_blank(),text = element_text(size = 15),
+    # axis.ticks.x=element_blank(),
+    # panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    # panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    # legend.position = 'bottom',legend.justification='center',
+    # legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
 dev.off()
 
