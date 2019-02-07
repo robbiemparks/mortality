95eb41a6d734e7f111f5e993ad7e958939831468
rmp15 <rparks15@imperial.ac.uk>
2019-02-07 10:32:01 +0000

    updates to wavelet coding

--- USA/state/data/objects/objects.R
+++ USA/state/data/objects/objects.R
@@ -20,7 +20,7 @@ age.colours=c("blue",brewer.pal(9,"BrBG")[c(9:6,4:1)],"grey")
 f <- function(pal) brewer.pal(brewer.pal.info[pal, "maxcolors"], pal)
 mycols <- c(f("Dark2"), f("Set1")[1:8], f("Set2"), f("Set3"),"#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7", "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", "#8A7C64", "#599861" )
 #to make picking the number of the colour you want easier:
-#plot(1:length(mycols),col=mycols[1:length(mycols)],cex=4,pch=20); abline(v=c(10,20,30,40,50,60))
+# plot(1:length(mycols),col=mycols[1:length(mycols)],cex=4,pch=20); abline(v=c(10,20,30,40,50,60))
 
 
 # colors for broad causes of death
