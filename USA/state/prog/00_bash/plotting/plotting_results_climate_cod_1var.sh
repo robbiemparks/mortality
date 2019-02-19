0ba1d654dee4c5920f31009db4cd1707fa6b3638
rmp15 <rparks15@imperial.ac.uk>
2019-02-19 15:01:55 +0000

    added new plots

--- USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
+++ USA/state/prog/00_bash/plotting/plotting_results_climate_cod_1var.sh
@@ -31,7 +31,7 @@ for metric in "${metrics[@]}"; do
 
 for model in "${models[@]}"; do
 
-Rscript ~/git/mortality/USA/state/prog/bind_posterior/bind_posterior_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig $pw
+#Rscript ~/git/mortality/USA/state/prog/bind_posterior/bind_posterior_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig $pw
 Rscript ~/git/mortality/USA/state/prog/mapping_posterior/mapping_posterior_climate_cod_1var.R $start $end $country $model $dname $metric $cod $contig $pw
 
 #################################################
