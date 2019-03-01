525c6dacb62acfe874a52acf3e8e723b1b25e9b4
rmp15 <r.parks15@imperial.ac.uk>
2019-03-01 15:43:16 +0000

    Updating cardio draws file

--- USA/state/prog/00_bash/additional_deaths/additional_deaths_draws_cod_1var.sh
+++ USA/state/prog/00_bash/additional_deaths/additional_deaths_draws_cod_1var.sh
@@ -28,9 +28,11 @@ cd ~/git/mortality/USA/state/prog/00_bash/
 
 for model in "${models[@]}"; do
 
-echo "Preparing $model years $start - $end";
+echo "Running c$start - $end";
+
 
 Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_process_data_cod_1var.R $start $end $country $model $dname $metric $contig $draws;
+Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_cardio_stacked_climate_draws_process_data_cod_1var.R $start $end $country $model $dname $metric $contig $draws;
 
 done;
 
