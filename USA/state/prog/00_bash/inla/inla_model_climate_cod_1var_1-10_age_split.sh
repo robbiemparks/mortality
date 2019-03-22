ddcb00f89166133007af2d44567bac77d0b76bb6
rmp15 <r.parks15@imperial.ac.uk>
2019-03-22 16:58:08 +0000

    updated age split model

--- USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_1-10_age_split.sh
+++ USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_1-10_age_split.sh
@@ -41,6 +41,7 @@ declare cod="Cardiopulmonary"
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_age_split.R 65 1 $start $end 11 0 $dname $metric $start2 $end2 $cod $fast $contig $pw $break;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_age_split.R 0  1 $start $end 11 0 $dname $metric $start2 $end2 $cod $fast $contig $pw $break;
 
+# national temperature terms
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_age_split.R 65 2 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig $pw $break;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_age_split.R 0  2 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig $pw $break;
 Rscript ~/git/mortality/USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_age_split.R 65 1 $start $end 10 0 $dname $metric $start2 $end2 $cod $fast $contig $pw $break;
