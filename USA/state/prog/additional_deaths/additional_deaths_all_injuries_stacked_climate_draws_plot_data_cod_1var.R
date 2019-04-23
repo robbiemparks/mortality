959f4cc212845ff7a0d9d8de9928eb734a13574d
rmp15 <r.parks15@imperial.ac.uk>
2019-04-24 00:22:18 +0100

    added stuff

--- USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R
+++ USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R
@@ -94,7 +94,7 @@ ggplot() +
     # geom_bar(data=subset(additional.deaths.intent.summary,intent=='Unintentional'&sex.long%in%c('Male','Female')&age.long%in%age.print),aes(x=as.factor(age.long),y=deaths.added.mean),fill=NA,color='black',stat='identity') +
     # geom_errorbar(data=subset(additional.deaths.intent.summary,intent=='Unintentional'),aes(x=as.factor(age.long),ymax=deaths.added.ul,ymin=deaths.added.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Additional deaths associated with 1 degree \n additional warming (based on 2016 population)') +
+    xlab('Age group (years)') + ylab('Additional deaths associated with a 1 degree \n warmer year (based on 2016 population)') +
     # ylim(c(min.plot,max.plot)) +
     facet_wrap(~sex.long) +
     scale_fill_manual(values=colors.subinjuries[c(1,2,3,4)]) +
@@ -117,7 +117,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.intent.summary,intent=='Intentional'&sex.long!='Both'&age.long!='All ages'),aes(x=as.factor(age.long),y=deaths.added.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.summary,intent=='Intentional'),aes(x=as.factor(age.long),ymax=deaths.added.ul,ymin=deaths.added.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Additional deaths associated with 1 degree \n additional warming (based on 2016 population)') +
+    xlab('Age group (years)') + ylab('Additional deaths associated with a 1 degree \n warmer year (based on 2016 population)') +
     # ylim(c(min.plot,max.plot)) +
     facet_wrap(~sex.long) +
     scale_fill_manual(values=colors.subinjuries[c(5,6)]) +
@@ -140,7 +140,7 @@ p1 = ggplot() +
     geom_point(data=subset(additional.deaths.intent.summary,sex.long!='Both'&age.long!='All ages'),aes(x=as.factor(age.long),y=deaths.added.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.summary),aes(x=as.factor(age.long),ymax=deaths.added.ul,ymin=deaths.added.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Additional deaths associated with 1 degree \n additional warming (based on 2016 population)') +
+    xlab('Age group (years)') + ylab('Additional deaths associated with a 1 degree \n warmer year (based on 2016 population)') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(.~intent +sex.long) +
     scale_fill_manual(values=colors.subinjuries[c(1,2,3,5,6)]) +
@@ -168,7 +168,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.intent.monthly.summary,intent=='Unintentional'),aes(x=as.factor(month.short),y=deaths.added.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.monthly.summary,intent=='Unintentional'),aes(x=as.factor(month.short),ymax=deaths.added.ul,ymin=deaths.added.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Month') + ylab('Additional deaths associated with 1 degree \n additional warming (based on 2016 population)') +
+    xlab('Month') + ylab('Additional deaths associated with a 1 degree \n warmer year (based on 2016 population)') +
     # ylim(c(min.plot,max.plot)) +
     facet_wrap(~sex.long) +
     scale_fill_manual(values=colors.subinjuries[c(1,2,3,4)]) +
@@ -191,7 +191,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.intent.monthly.summary,intent=='Intentional'),aes(x=as.factor(month.short),y=deaths.added.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.monthly.summary,intent=='Intentional'),aes(x=as.factor(month.short),ymax=deaths.added.ul,ymin=deaths.added.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Month') + ylab('Additional deaths associated with 1 degree \n additional warming (based on 2016 population)') +
+    xlab('Month') + ylab('Additional deaths associated with a 1 degree \n warmer year (based on 2016 population)') +
     # ylim(c(min.plot,max.plot)) +
     facet_wrap(~sex.long) +
     scale_fill_manual(values=colors.subinjuries[c(5,6)]) +
@@ -214,7 +214,7 @@ p2 =ggplot() +
     geom_point(data=subset(additional.deaths.intent.monthly.summary),aes(x=as.factor(month.short),y=deaths.added.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.monthly.summary),aes(x=as.factor(month.short),ymax=deaths.added.ul,ymin=deaths.added.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Month') + ylab('Additional deaths associated with 1 degree \n additional warming (based on 2016 population)') +
+    xlab('Month') + ylab('Additional deaths associated with a 1 degree \n warmer year (based on 2016 population)') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(. ~intent + sex.long) +
     scale_fill_manual(values=colors.subinjuries[c(1,2,3,5,6)]) +
@@ -313,7 +313,7 @@ p3$widths[2:3] = maxWidth ; p4$widths[2:3] = maxWidth ; p5$widths[2:3] = maxWidt
 # everything all on one page
 pdf(paste0(file.loc,country,'_rate_pred_type',model,
     '_',year.start,'_',year.end,'_',dname,'_',metric,'_intentional_unintentional_all_contig.pdf'),paper='a4r',height=0,width=0)
-grid.arrange(p3,p4,nrow=2,left='Additional deaths associated with 1 degree additional warming (based on 2016 population)')
+grid.arrange(p3,p4,nrow=2,left='Additional deaths associated with a 1 degree warmer year (based on 2016 population)')
 dev.off()
 
 #name=expression(paste("Temperature (",degree,"C)"))
@@ -321,7 +321,7 @@ dev.off()
 # same plot as above but with skipping month names
 pdf(paste0(file.loc,country,'_rate_pred_type',model,
     '_',year.start,'_',year.end,'_',dname,'_',metric,'_intentional_unintentional_all_contig_month_skip.pdf'),paper='a4r',height=0,width=0)
-grid.arrange(p3,p5,nrow=2,left='Additional deaths associated with 1 degree additional warming (based on 2016 population)')
+grid.arrange(p3,p5,nrow=2,left='Additional deaths associated with a 1 degree warmer year (based on 2016 population)')
 dev.off()
 
 # PLOTS IN RELATIVE RISK IN DEATHS
@@ -401,7 +401,7 @@ ggplot() +
     # geom_point(data=subset(additional.deaths.intent.summary.perc,intent=='1. Unintentional'),aes(x=as.factor(age.long),y=perc.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.summary.perc,intent=='1. Unintentional'),aes(x=as.factor(age.long),ymax=perc.ul,ymin=perc.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Age group (years)') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(cause~sex.long) +
     scale_y_continuous(labels=scales::percent) +
@@ -428,7 +428,7 @@ ggplot() +
     # geom_point(data=subset(additional.deaths.intent.summary.perc,intent=='1. Unintentional'),aes(x=as.factor(age.long),y=perc.mean),shape=16) +
     # geom_errorbar(data=subset(additional.deaths.intent.summary.perc,intent=='1. Unintentional'),aes(x=as.factor(age.long),ymax=perc.ul,ymin=perc.ll),width=.3,size=0.5) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Age group (years)') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(cause~sex.long) +
     scale_y_continuous(labels=scales::percent) +
@@ -455,7 +455,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.summary.perc,sex>0&age<99&cause!='Other unintentional injuries'), aes(x=as.factor(age.long),y=perc.mean),size=3,shape=16) +
     geom_point(data=subset(additional.deaths.summary.perc,sex>0&age<99&cause!='Other unintentional injuries'), aes(x=as.factor(age.long),y=perc.mean,color=cause),size=2,shape=16) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Age group (years)') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     facet_grid(cause~sex.long) +
     scale_y_continuous(labels=scales::percent_format(accuracy=1)) +
     scale_color_manual(values=colors.subinjuries[c(1,2,3,5,6)]) +
@@ -480,7 +480,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.summary.perc,sex>0&age<99),position=position_dodge(width=0.5), aes(x=as.factor(age.long),y=perc.mean,group=cause),size=3,shape=16) +
     geom_point(data=subset(additional.deaths.summary.perc,sex>0&age<99),position=position_dodge(width=0.5), aes(x=as.factor(age.long),y=perc.mean,color=cause),size=2,shape=16) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Age group (years)') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(~sex.long) +
     scale_y_continuous(labels=scales::percent) +
@@ -506,7 +506,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.summary.perc,sex>0&age<99&cause!='Other unintentional injuries'), aes(x=as.factor(age.long),y=perc.mean),size=3,shape=16) +
     geom_point(data=subset(additional.deaths.summary.perc,sex>0&age<99&cause!='Other unintentional injuries'), aes(x=as.factor(age.long),y=perc.mean,color=cause),size=2,shape=16) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Age group (years)') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Age group (years)') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(cause~sex.long,scale='free') +
     scale_y_continuous(labels=scales::percent) +
@@ -551,7 +551,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.summary.monthly.perc,sex>0&month<99&cause!='Other unintentional injuries'), aes(x=as.factor(month.short),y=perc.mean),size=3,shape=16) +
     geom_point(data=subset(additional.deaths.summary.monthly.perc,sex>0&month<99&cause!='Other unintentional injuries'), aes(x=as.factor(month.short),y=perc.mean,color=cause),size=2,shape=16) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Month') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Month') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     coord_flip() +
     facet_grid(cause~sex.long) +
@@ -579,7 +579,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.summary.monthly.perc,sex>0&month<99), aes(x=as.factor(month.short),y=perc.mean),size=3,shape=16) +
     geom_point(data=subset(additional.deaths.summary.monthly.perc,sex>0&month<99), aes(x=as.factor(month.short),y=perc.mean,color=cause),size=2,shape=16) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Month') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Month') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(cause~sex.long,scales='free') +
     scale_y_continuous(labels=scales::percent) +
@@ -603,7 +603,7 @@ ggplot() +
     geom_point(data=subset(additional.deaths.summary.monthly.perc,sex>0&month<99),position=position_dodge(width=0.5), aes(x=month.short,y=perc.mean,group=cause),size=3,shape=16) +
     geom_point(data=subset(additional.deaths.summary.monthly.perc,sex>0&month<99),position=position_dodge(width=0.5), aes(x=month.short,y=perc.mean,color=cause),size=2,shape=16) +
     geom_hline(yintercept=0,linetype='dotted') +
-    xlab('Month') + ylab('Excess relative risk associated with 1 degree additional warming') +
+    xlab('Month') + ylab('Excess relative risk associated with a 1 degree warmer year') +
     # ylim(c(min.plot,max.plot)) +
     facet_grid(~sex.long) +
     scale_y_continuous(labels=scales::percent) +
