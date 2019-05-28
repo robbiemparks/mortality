4fe448ae0111ca1ca7aa281398f29f59bc855af5
rmp15 <r.parks15@imperial.ac.uk>
2019-05-28 18:18:17 +0100

    comparing subnational and national cardio

---
+++ USA/state/prog/additional_deaths/additional_deaths_all_cardio_stacked_climate_draws_compare_data_cod_1var.R
@@ -0,0 +1,81 @@
+rm(list=ls())
+
+library(RColorBrewer)
+library(ggplot2)
+library(plyr)
+library(scales)
+library(gridExtra)
+library(grid)
+
+# break down the arguments from Rscript
+args <- commandArgs(trailingOnly=TRUE)
+year.start <- as.numeric(args[1])
+year.end <- as.numeric(args[2])
+country <- as.character(args[3])
+model <- as.numeric(args[4])
+dname <- as.character(args[5])
+metric <- as.character(args[6])
+contig <- as.numeric(args[7])
+num.draws <- as.numeric(args[8])
+
+# NEED TO MAKE CONTIG OPTION ACTUALLY DO SOMETHING
+
+#year.start = 1980 ; year.end = 2016 ; country = 'USA' ; model = 10 ; dname = 't2m' ; metric = 'meanc3' ; contig=1 ; num.draws = 5000
+#year.start = 1980 ; year.end = 2016 ; country = 'USA' ; model = 11 ; dname = 't2m' ; metric = 'meanc3' ; contig=1 ; num.draws = 1000
+
+# source variables
+source('../../data/objects/objects.R')
+
+# Load national model values
+year.start = 1980 ; year.end = 2016 ; country = 'USA' ; model = 10 ; dname = 't2m' ; metric = 'meanc3' ; contig=1 ; num.draws = 5000
+model <- models[model]
+
+file.loc <- paste0('../../output/additional_deaths_climate/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/type_',model,'/non_contig/all_cardio/',num.draws,'_draws/')
+if(contig==1){
+    file.loc <- paste0('../../output/additional_deaths_climate/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/type_',model,'/contig/all_cardio/',num.draws,'_draws/')
+}
+ifelse(!dir.exists(file.loc), dir.create(file.loc,recursive=TRUE), FALSE)
+
+additional.deaths = readRDS(paste0(file.loc,'additional_deaths_age_draws.rds'))
+additional.deaths.cause.together = ddply(additional.deaths,.(sex,age,draw),summarize,deaths.added=sum(deaths.added))
+additional.deaths.cause.together = ddply(additional.deaths.cause.together,.(sex,age),summarize, deaths.added.median=median(deaths.added),deaths.added.mean=mean(deaths.added),deaths.added.ll=quantile(deaths.added,0.025),deaths.added.ul=quantile(deaths.added,0.975))
+additional.deaths.cause.together = additional.deaths.cause.together[,c(1,2,4,5,6)]
+names(additional.deaths.cause.together) = c('sex','age','deaths.added.mean.nat','deaths.added.ll.nat','deaths.added.ul.nat')
+
+# load subnational model values
+year.start = 1980 ; year.end = 2016 ; country = 'USA' ; model = 11 ; dname = 't2m' ; metric = 'meanc3' ; contig=1 ; num.draws = 1000
+model <- models[model]
+
+file.loc <- paste0('../../output/additional_deaths_climate/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/type_',model,'/non_contig/all_cardio/',num.draws,'_draws/')
+if(contig==1){
+    file.loc <- paste0('../../output/additional_deaths_climate/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/type_',model,'/contig/all_cardio/',num.draws,'_draws/')
+}
+ifelse(!dir.exists(file.loc), dir.create(file.loc,recursive=TRUE), FALSE)
+
+additional.deaths = readRDS(paste0(file.loc,'additional_deaths_age_draws.rds'))
+additional.deaths.cause.together.subnational = ddply(additional.deaths,.(sex,age,draw),summarize,deaths.added=sum(deaths.added))
+additional.deaths.cause.subnational = ddply(additional.deaths.cause.together.subnational,.(sex,age),summarize, deaths.added.median=median(deaths.added),deaths.added.mean=mean(deaths.added),deaths.added.ll=quantile(deaths.added,0.025),deaths.added.ul=quantile(deaths.added,0.975))
+additional.deaths.cause.subnational = additional.deaths.cause.subnational[,c(1,2,4,5,6)]
+names(additional.deaths.cause.subnational) = c('sex','age','deaths.added.mean.sub','deaths.added.ll.sub','deaths.added.ul.sub')
+
+# merge national and subnational dataset then plot to compare
+additional.deaths.compare = merge(additional.deaths.cause.together,additional.deaths.cause.subnational)
+
+ggplot(data=additional.deaths.compare,aes(x=deaths.added.mean.nat,y=deaths.added.mean.sub)) +
+    geom_point() +
+    geom_errorbar(aes(x=deaths.added.mean.nat,ymin=deaths.added.ll.nat,ymax=deaths.added.ul.nat)) +
+    geom_errorbarh(aes(xmin=deaths.added.ll.sub,xmax=deaths.added.ul.sub)) +
+    geom_abline(linetype='dotted') +
+    coord_equal() +
+    xlab('Change in deaths estimated from national model') + ylab('Change in deaths estimated from subnational model') +
+    theme_bw() + theme(text = element_text(size = 15),
+    panel.grid.major = element_blank(),axis.text.x = element_text(angle=0),
+    plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
