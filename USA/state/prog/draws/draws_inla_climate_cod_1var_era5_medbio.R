7b000d3c2fd78fad62f600f19ff839a54ac77413
rmp15 <r.parks15@imperial.ac.uk>
2019-09-30 11:33:10 -0400

    A

--- USA/state/prog/draws/draws_inla_climate_cod_1var_era5_medbio.R
+++ USA/state/prog/draws/draws_inla_climate_cod_1var_era5_medbio.R
@@ -1,25 +1,29 @@
 rm(list=ls())
 
-library(RColorBrewer)
-library(ggplot2)
-library(plyr)
-library(scales)
+# set up argument (don't get why but it should be like this apparently)
+seedVal <-as.numeric(commandArgs()[4])
+
+# create complete grid of age, sex, and cause of death values
+sexes = c(1,2)
+ages = c(0,5,15,25,35,45,55,65,75,85)
+causes = c('Transport accidents','Accidental falls','Accidental drowning and submersion','Intentional self-harm','Assault')
+
+seed.grid = expand.grid(sex=sexes,age=ages,cause=causes)
+
+chosen.row =seed.grid[seedVal,]
 
 # break down the arguments from Rscript
-args <- commandArgs(trailingOnly=TRUE)
-year.start <- as.numeric(args[1])
-year.end <- as.numeric(args[2])
-country <- as.character(args[3])
-model <- as.numeric(args[4])
-dname <- as.character(args[5])
-metric <- as.character(args[6])
-cause <- as.character(args[7]) ; cause <- gsub('_',' ',cause)
-contig <- as.numeric(args[8])
-num.draws <- as.numeric(args[9])
-
-# NEED TO MAKE CONTIG OPTION ACTUALLY DO SOMETHING
-
-#year.start = 1980 ; year.end = 2017 ; country = 'USA' ; model = 27 ; dname = 't2m' ; metric = 'meanc4' ; cause = 'Transport accidents'; contig=1 ; num.draws = 1000
+sex <- as.numeric(chosen.row[1,1])
+age <- as.numeric(chosen.row[1,2])
+cause <- as.character(chosen.row[1,3])
+year.start <- 1980
+year.end <- 2017
+country <- 'USA'
+model <- 27
+dname <- 't2m'
+metric <- 'meanc4'
+contig <- 1
+num.draws <- 1000
 
 # source variables
 source('../../data/objects/objects.R')
@@ -31,82 +35,43 @@ INLA:::inla.dynload.workaround()
 
 # load inla paradiso (what on earth is this?)
 library(INLA)
-inla.paradiso()
-inla.setOption(pardiso.license="~/git/mortality/USA/state/prog/00_bash/paradiso.lic")
-inla.paradiso.check()
 
-# temporary list of injury causes
-causes = c("Transport accidents", "Intentional_self-harm", "Accidental falls", "Accidental drowning and submersion", "Assault")
+# create directories for output
+file.loc <- paste0('~/data/mortality/US/state/draws_era5/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/type_',model,'/non_contig/',cause,'/',num.draws,'_draws/age_groups/',age.filter[age],'/')
+if(contig==1){
+    file.loc <- paste0('~/data/mortality/US/state/draws_era5/',year.start,'_',year.end,
+    '/',dname,'/',metric,'/non_pw/type_',model,'/contig/',cause,'/',num.draws,'_draws/age_groups/',age.filter[age],'/')
+    }
+ifelse(!dir.exists(file.loc), dir.create(file.loc,recursive=TRUE), FALSE)
 
-# load the data for each age and sex to make draws
-# for(cause in causes){
-for (i in seq(length(sex.filter))) {
-    for (j in seq(length(age.filter))) {
+# load the full model for a particular age and sex
+if(cause!='AllCause'){
+    file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
+    dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age,
+    '/',country,'_rate_pred_type',model,'_',age,'_',sex.lookup[sex],'_',
+    year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
+}
+if(cause=='AllCause'){
+    file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
+    dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age,
+    '/',country,'_rate_pred_type',model,'_',age,'_',sex.lookup[sex],
+    '_',year.start,'_',year.end,'_',dname,'_',metric,'_parameters_fast_contig')
+}
 
-        # create directories for output
-        file.loc <- paste0('~/data/mortality/US/state/draws_era5/',year.start,'_',year.end,
-        '/',dname,'/',metric,'/non_pw/type_',model,'/non_contig/',cause,'/',num.draws,'_draws/age_groups/',age.filter[j],'/')
-        if(contig==1){
-            file.loc <- paste0('~/data/mortality/US/state/draws_era5/',year.start,'_',year.end,
-            '/',dname,'/',metric,'/non_pw/type_',model,'/contig/',cause,'/',num.draws,'_draws/age_groups/',age.filter[j],'/')
-            }
-        ifelse(!dir.exists(file.loc), dir.create(file.loc,recursive=TRUE), FALSE)
+print(paste0('Reading ',file.name))
+model.current <- readRDS(file.name)
 
-        # load the full model for a particular age and sex
-        if(cause!='AllCause'){
-            file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
-            dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age.filter[j],
-            '/',country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],'_',
-            year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
-        }
-        if(cause=='AllCause'){
-            file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
-            dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age.filter[j],
-            '/',country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],
-            '_',year.start,'_',year.end,'_',dname,'_',metric,'_parameters_fast_contig')
-        }
+# make draws from the model for the parameters
+print(paste0('Making ',num.draws, ' draws...'))
+draws.current = try(inla.posterior.sample(num.draws,model.current))
 
-        print(paste0('Reading ',file.name))
-        model.current <- readRDS(file.name)
+# OTHER ATTEMPTS
+# draws.current = try(inla.posterior.sample(num.draws,model.current,selection=list('Predictor'=1:10)))
+# try(do.call("<-", list(paste0('draws.',age.filter[j],'.',sex.lookup[i]), draws.current)))
 
-        # make draws from the model for the parameters
-        print(paste0('Making ',num.draws, ' draws...'))
-        # draws.current = try(inla.posterior.sample(num.draws,model.current))
-        draws.current = try(inla.posterior.sample(num.draws,model.current,selection=list('Predictor'=1:10)))
-        # try(do.call("<-", list(paste0('draws.',age.filter[j],'.',sex.lookup[i]), draws.current)))
-
-        # save draws as an rds file
-        print('Saving file...')
-        save.name = paste0(country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],
-            '_',year.start,'_',year.end,'_',dname,'_',metric,'_',num.draws,'_draws_fast_contig')
-        try(saveRDS(draws.current,paste0(file.loc,save.name)))
-}}
-# }
-### LEGACY ONLY FOR COMPARISON OF CENTRAL ESTIMATES
-
-# # load the data for quoting parameters
-# if(contig==1){
-#     if(cause!='AllCause'){
-#         dat <- readRDS(paste0('../../data/climate_effects/',dname,'/',metric,'/non_pw/type_',model,'/parameters/',
-#         country,'_rate_pred_type',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_fast_contig'))
-#     }
-#     if(cause=='AllCause'){
-#         dat <- readRDS(paste0('../../data/climate_effects/',dname,'/',metric,'/non_pw/type_',model,'/parameters/'
-#         ,country,'_rate_pred_type',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_fast_contig'))
-#     }
-# }
-# if(contig==0){
-#     if(cause!='AllCause'){
-#         dat <- readRDS(paste0('../../data/climate_effects/',dname,'/',metric,'/non_pw/type_',model,'/parameters/',
-#         country,'_rate_pred_type',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_fast'))
-#     }
-#     if(cause=='AllCause'){
-#         dat <- readRDS(paste0('../../data/climate_effects/',dname,'/',metric,'/non_pw/type_',model,'/parameters/'
-#         ,country,'_rate_pred_type',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_fast'))
-#     }
-# }
-
-# to check distribution of parameters if required
-# ggplot() + geom_point(data=subset(dat,age==25&sex==1),aes(x=as.factor(ID),y=odds.mean,color='red',size=5)) +
-#     geom_boxplot(data=complete.table,aes(x=as.factor(ID),y=climate.values)) +
-#     geom_point(data=subset(dat,age==25&sex==1),aes(x=as.factor(ID),y=odds.mean,color='red',size=5))
+# save draws as an rds file
+print('Saving file...')
+save.name = paste0(country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],
+    '_',year.start,'_',year.end,'_',dname,'_',metric,'_',num.draws,'_draws_fast_contig')
+try(saveRDS(draws.current,paste0(file.loc,save.name)))
