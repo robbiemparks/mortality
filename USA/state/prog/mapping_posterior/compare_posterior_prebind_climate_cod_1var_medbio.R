e655dcf2458e96371740ffb153187c40851768c1
rmp15 <r.parks15@imperial.ac.uk>
2019-10-03 10:07:03 -0400

    A

--- USA/state/prog/mapping_posterior/compare_posterior_prebind_climate_cod_1var_medbio.R
+++ USA/state/prog/mapping_posterior/compare_posterior_prebind_climate_cod_1var_medbio.R
@@ -28,72 +28,74 @@ if(pw.arg==0){
     dat <- data.frame()
     # create dataframe of all the actual parameter terms
     dat.parameters <- data.frame()
-    
+
     # find the posterior exponential mean
-    # for (i in seq(length(sex.filter))) {
-    #     for (j in seq(length(age.filter))) {
-    for (i in c(1)) {
-        for (j in c(2,3,4,5,6,8,9,10)) {
+for (cause in c('Transport accidents','Accidental falls','Accidental drowning and submersion','Intentional self-harm','Assault')){
+    for (i in seq(length(sex.filter))) {
+        for (j in seq(length(age.filter))) {
             # load data
-            print(paste0(i,' ',j))
+            print(paste0(cause,' ',i,' ',j))
 
             if(cause!='AllCause'){
-                file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
+                file.name <- paste0('/rds/general/user/rmp15/ephemeral/data/mortality/US/state/climate_effects_era5/',
                 dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age.filter[j],
                 '/',country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],'_',
                 year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
-                file.name.2 <- paste0('~/data/mortality/US/state/climate_effects_era5/',
+                file.name.2 <- paste0('/rds/general/user/rmp15/ephemeral/data/mortality/US/state/climate_effects_era5/',
                 dname,'/',metric,'/non_pw/type_',model.2,'/age_groups/',age.filter[j],
                 '/',country,'_rate_pred_type',model.2,'_',age.filter[j],'_',sex.lookup[i],'_',
                 year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
             }
             if(cause=='AllCause'){
-                file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
+                file.name <- paste0('/rds/general/user/rmp15/ephemeral/data/mortality/US/state/climate_effects_era5/',
                 dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age.filter[j],
                 '/',country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],
                 '_',year.start,'_',year.end,'_',dname,'_',metric,'_parameters_fast_contig')
-                file.name.2 <- paste0('~/data/mortality/US/state/climate_effects_era5/',
+                file.name.2 <- paste0('/rds/general/user/rmp15/ephemeral/data/mortality/US/state/climate_effects_era5/',
                 dname,'/',metric,'/non_pw/type_',model.2,'/age_groups/',age.filter[j],
                 '/',country,'_rate_pred_type',model.2,'_',age.filter[j],'_',sex.lookup[i],
                 '_',year.start,'_',year.end,'_',dname,'_',metric,'_parameters_fast_contig')
             }
 
             # load parameters
-            model.current <- readRDS(file.name)
-            model.current.2 <- readRDS(file.name.2)
+            try(model.current <- readRDS(file.name))
+            print('loaded 1st')
+            try(model.current.2 <- readRDS(file.name.2))
+            print('loaded 2nd')
 
             # parameters of each model
-            dat.parameters.1 <- data.frame(model.1.mean=model.current$summary.random$month5$mean,model.1.ll=model.current$summary.random$month5$`0.025quant`,
-                                            model.1.ul=model.current$summary.random$month5$`0.975quant`)
-            dat.parameters.2 <- data.frame(model.2.mean=model.current.2$summary.random$month5$mean,model.2.ll=model.current.2$summary.random$month5$`0.025quant`,
-                                            model.2.ul=model.current.2$summary.random$month5$`0.975quant`)
-            dat.param.current <- cbind(dat.parameters.1, dat.parameters.2)
-            dat.param.current$age=age.filter[j] ; dat.param.current$sex=i
+            try(dat.parameters.1 <- data.frame(model.1.mean=model.current$summary.random$month5$mean,model.1.ll=model.current$summary.random$month5$`0.025quant`,
+                                            model.1.ul=model.current$summary.random$month5$`0.975quant`))
+            try(dat.parameters.2 <- data.frame(model.2.mean=model.current.2$summary.random$month5$mean,model.2.ll=model.current.2$summary.random$month5$`0.025quant`,
+                                            model.2.ul=model.current.2$summary.random$month5$`0.975quant`))
+            try(dat.param.current <- cbind(dat.parameters.1, dat.parameters.2))
+            try(dat.param.current$age <- age.filter[j]) ; try(dat.param.current$sex <-i) ; try(dat.param.current$cause <-cause)
 
-            dat.parameters <- rbind(dat.parameters,dat.param.current)
+            try(dat.parameters <- rbind(dat.parameters,dat.param.current))
 
-            corr.temp.terms=cor(model.current$summary.random$month5$mean,model.current.2$summary.random$month5$mean)
+            # corr.temp.terms=cor(model.current$summary.random$month5$mean,model.current.2$summary.random$month5$mean)
+            #
+            # corr.state.intercept.terms=cor(model.current$summary.random$ID$mean,model.current.2$summary.random$ID$mean)
+            # corr.month.intercept.terms=cor(model.current$summary.random$month$mean,model.current.2$summary.random$month$mean)
+            # corr.state.month.intercept.terms=cor(model.current$summary.random$month3$mean,model.current.2$summary.random$month3$mean)
+            #
+            # corr.state.slope.terms=cor(model.current$summary.random$ID2$mean,model.current.2$summary.random$ID2$mean)
+            # corr.month.slope.terms=cor(model.current$summary.random$month2$mean,model.current.2$summary.random$month2$mean)
+            # corr.state.month.slope.terms=cor(model.current$summary.random$month4$mean,model.current.2$summary.random$month4$mean)
+            #
+            # scale.month7 = mean(100*model.current.2$summary.random$month7$mean / mean(model.current$summary.random$month3$mean))
+            #
+            # # obtain correlation values
+            # corr.current = data.frame(age=age.filter[j],sex=i,corr.temp.terms=corr.temp.terms, corr.state.intercept.terms= corr.state.intercept.terms,
+            # corr.month.intercept.terms=corr.month.intercept.terms,corr.state.month.intercept.terms=corr.state.month.intercept.terms,
+            # corr.state.slope.terms=corr.state.slope.terms,corr.month.slope.terms=corr.month.slope.terms,corr.state.month.slope.terms=corr.state.month.slope.terms,
+            # scale.new.temp.slope=scale.month7)
 
-            corr.state.intercept.terms=cor(model.current$summary.random$ID$mean,model.current.2$summary.random$ID$mean)
-            corr.month.intercept.terms=cor(model.current$summary.random$month$mean,model.current.2$summary.random$month$mean)
-            corr.state.month.intercept.terms=cor(model.current$summary.random$month3$mean,model.current.2$summary.random$month3$mean)
-
-            corr.state.slope.terms=cor(model.current$summary.random$ID2$mean,model.current.2$summary.random$ID2$mean)
-            corr.month.slope.terms=cor(model.current$summary.random$month2$mean,model.current.2$summary.random$month2$mean)
-            corr.state.month.slope.terms=cor(model.current$summary.random$month4$mean,model.current.2$summary.random$month4$mean)
-
-            scale.month7 = mean(100*model.current.2$summary.random$month7$mean / mean(model.current$summary.random$month3$mean))
-
-            # obtain correlation values
-            corr.current = data.frame(age=age.filter[j],sex=i,corr.temp.terms=corr.temp.terms, corr.state.intercept.terms= corr.state.intercept.terms,
-            corr.month.intercept.terms=corr.month.intercept.terms,corr.state.month.intercept.terms=corr.state.month.intercept.terms,
-            corr.state.slope.terms=corr.state.slope.terms,corr.month.slope.terms=corr.month.slope.terms,corr.state.month.slope.terms=corr.state.month.slope.terms,
-            scale.new.temp.slope=scale.month7)
-
-            dat = rbind(dat,corr.current)
+            # dat = rbind(dat,corr.current)
 
         }
     }
+}
 
 # create directories for output
 file.loc.git <- paste0('../../output/compare_posterior_climate_era5/',year.start,'_',year.end,'/',dname,'/',metric,'/non_pw/type_',model,'_and_',model.2,'/parameters/')
