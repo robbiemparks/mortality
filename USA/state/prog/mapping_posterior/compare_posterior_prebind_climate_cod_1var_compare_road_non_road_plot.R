cea757fae55e6411ba484582d94601d43f047579
rmp15 <r.parks15@imperial.ac.uk>
2019-11-05 13:55:54 +0000

    A

--- USA/state/prog/mapping_posterior/compare_posterior_prebind_climate_cod_1var_compare_road_non_road_plot.R
+++ USA/state/prog/mapping_posterior/compare_posterior_prebind_climate_cod_1var_compare_road_non_road_plot.R
@@ -15,76 +15,62 @@ pw.arg <- as.numeric(args[11])
 
 # for model testing
 # year.start = 1980 ; year.end = 2017 ; country = 'USA' ; model = 27 ; dname='t2m' ; metric='meanc4'
-# cause = 'Transport accidents' ; cause.2 = 'Road traffic accidents' ;  contig.arg = 1 ; pw.arg = 0
-# Other transport accidents
+# cause = 'Transport accidents' ; cause.2 = 'Road traffic accidents' ;  cause.3 = 'Other transport accidents'
+# contig.arg = 1 ; pw.arg = 0
 
 # source variables
 source('../../data/objects/objects.R')
 model <- models[model]
 
-library(INLA)
+# library(INLA)
 
 # create dataframe with each of the national terms for entire group of age and sexes
 dat <- data.frame()
 # create dataframe of all the actual parameter terms
 dat.parameters <- data.frame()
 
-    # find the posterior exponential mean
-for (i in c(1)) {
-    for (j in c(1,6,7,8,10)) {
-        # load data
-        print(paste0(cause,' ',cause.2,' ',i,' ',j))
-
-        # load file names
-        # file.name <- paste0('~/data/mortality/US/state/climate_effects_era5/',
-        # dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age.filter[j],
-        # '/',country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],'_',
-        # year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
-        file.name.2 <- paste0('/rds/general/user/rmp15/ephemeral/data/mortality/US/state/climate_effects_era5/',
-        dname,'/',metric,'/non_pw/type_',model,'/age_groups/',age.filter[j],
-        '/',country,'_rate_pred_type',model,'_',age.filter[j],'_',sex.lookup[i],'_',
-        year.start,'_',year.end,'_',dname,'_',metric,'_',cause.2,'_parameters_fast_contig')
-
-
-        # print(file.exists(file.name))
-        print(file.exists(file.name.2))
-        #
-        # # # if(test.1==TRUE&test.2==TRUE){
-        # #
-        # # load parameters
-        # try(model.current <- readRDS(file.name))
-        # print('loaded 1st')
-        try(model.current.2 <- readRDS(file.name.2))
-        # print('loaded 2nd')
-        #
-        # # parameters of each model
-        # try(dat.parameters.1 <- data.frame(model.1.mean=model.current$summary.random$month5$mean,model.1.ll=model.current$summary.random$month5$`0.025quant`,
-        #                                 model.1.ul=model.current$summary.random$month5$`0.975quant`))
-        try(dat.parameters.2 <- data.frame(model.2.mean=model.current.2$summary.random$month5$mean,model.2.ll=model.current.2$summary.random$month5$`0.025quant`,
-                                        model.2.ul=model.current.2$summary.random$month5$`0.975quant`))
-        try(dat.param.current <- cbind(dat.parameters.2))
-        try(dat.param.current$age <- age.filter[j]) ; try(dat.param.current$sex <-i) ; try(dat.param.current$cause <-cause.2)
-        #
-        try(dat.parameters <- rbind(dat.parameters,dat.param.current))
-
-        # }
-
-    }
-}
-
-# create directories for output
+# output file location to recover data from
 file.loc.git <- paste0('../../output/compare_posterior_climate_era5/',year.start,'_',year.end,'/',dname,'/',metric,'/non_pw/type_',model,'_road_non_road_comparison/parameters/')
-ifelse(!dir.exists(file.loc.git), dir.create(file.loc.git, recursive=TRUE), FALSE)
 
-# save bound posterior and summaries
-# save.name <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause.2,'_fast_contig.csv')
-save.name.param <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause.2,'_',i,'_fast_contig.csv')
+# load files
+save.name.param.1 <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_1_fast_contig.csv')
+dat.1 = read.csv(paste0(file.loc.git,save.name.param.1))
+dat.1$X = NULL
 
-# write.csv(dat,paste0(file.loc.git,save.name))
-write.csv(dat.parameters,paste0(file.loc.git,save.name.param))
+save.name.param.2 <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_2_fast_contig.csv')
+dat.2 = read.csv(paste0(file.loc.git,save.name.param.2))
+dat.2$X = NULL
 
-# # plot results
-# library(ggplot2)
+save.name.param.3 <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause.2,'_1_fast_contig.csv')
+dat.3 = read.csv(paste0(file.loc.git,save.name.param.3))
+dat.3$X = NULL
+
+save.name.param.4 <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause.2,'_2_fast_contig.csv')
+dat.4 = read.csv(paste0(file.loc.git,save.name.param.4))
+dat.4$X = NULL
+
+save.name.param.5 <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause.3,'_1_fast_contig.csv')
+dat.5 = read.csv(paste0(file.loc.git,save.name.param.5))
+dat.5$X = NULL
+
+save.name.param.6 <- paste0(country,'_parameters_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause.3,'_2_fast_contig.csv')
+dat.6 = read.csv(paste0(file.loc.git,save.name.param.6))
+dat.6$X = NULL
+
+# combine male and female for each
+dat.transport = rbind(dat.1,dat.2)
+dat.road = rbind(dat.3,dat.4)
+dat.nonroad = rbind(dat.5,dat.6)
+
+# add month ID
+dat.transport$month = seq(1:12) ; dat.road$month = seq(1:12) ; dat.nonroad$month = seq(1:12)
+
+# combine all files to compare
+dat.road.compare = merge(dat.transport,dat.road,by=c('month','age','sex'),all.x=TRUE)
+dat.nonroad.compare = merge(dat.transport,dat.nonroad,by=c('month','age','sex'),all.x=TRUE)
+
+# plot results
+library(ggplot2)
 #
 # dat.parameters$sex.long <- plyr::mapvalues(dat.parameters$sex,from=sort(unique(dat.parameters$sex)),to=c('Male','Female'))
 # dat.parameters$sex.long <- with(dat.parameters,reorder(dat.parameters$sex.long,sex))
@@ -92,55 +78,41 @@ write.csv(dat.parameters,paste0(file.loc.git,save.name.param))
 # dat.parameters$age.long <- plyr::mapvalues(dat.parameters$age,from=sort(unique(dat.parameters$age)),to=as.character(age.code[,2]))
 # dat.parameters$age.long <- reorder(dat.parameters$age.long,dat.parameters$age)
 #
-# pdf(paste0(file.loc.git,country,'_correlations_',model,'_',model.2,'_',year.start,'_',year.end,'_',dname,'_',metric,'_fast_contig.pdf'),paper='a4r',height=0,width=0)
-# print(ggplot(data=dat.parameters,aes(x=model.1.mean,y=model.2.mean)) +
-#     geom_point() +
-#     # geom_errorbar(aes(ymin=model.1.ll,ymax=model.1.ul)) +
-#     # geom_errorbarh(aes(xmin=model.2.ll,xmax=model.2.ul)) +
-#     geom_abline() +
-#     coord_equal() +
-#     xlab('Temperature parameters from original model') + ylab('Temperature parameters from model\nwith adjusted hyperpriors') +
-#     theme_bw() + theme(text = element_text(size = 15),
-#     panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
-#     plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
-#     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-#     panel.border = element_rect(colour = "black"),strip.background = element_blank(),
-#     legend.position = 'bottom',legend.justification='center',
-#     legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
-# )
-#
-# print(ggplot(data=dat.parameters) +
-#     geom_point(aes(x=model.1.mean,y=model.2.mean)) +
-#     # geom_errorbar(aes(ymin=model.1.ll,ymax=model.1.ul)) +
-#     # geom_errorbarh(aes(xmin=model.2.ll,xmax=model.2.ul)) +
-#     geom_abline() +
-#     coord_equal() +
-#     xlab('Temperature parameters from original model') + ylab('Temperature parameters from model\nwith adjusted hyperpriors') +
-#     facet_grid(sex~age) +
-#     theme_bw() + theme(text = element_text(size = 15),
-#     panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
-#     plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
-#     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-#     panel.border = element_rect(colour = "black"),strip.background = element_blank(),
-#     legend.position = 'bottom',legend.justification='center',
-#     legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
-# )
-#
-# print(ggplot(data=dat.parameters) +
-#     geom_point(aes(x=model.1.mean,y=model.2.mean)) +
-#     geom_errorbar(aes(x=model.1.mean,ymin=model.1.ll,ymax=model.1.ul)) +
-#     geom_errorbarh(aes(x=model.1.mean,xmin=model.2.ll,xmax=model.2.ul,y=model.2.mean)) +
-#     geom_abline() +
-#     coord_equal() +
-#     xlab('Temperature parameters from original model') + ylab('Temperature parameters from model\nwith adjusted hyperpriors') +
-#     # facet_grid(sex.long~age.long) +
-#     theme_bw() + theme(text = element_text(size = 15),
-#     panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
-#     plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
-#     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-#     panel.border = element_rect(colour = "black"),strip.background = element_blank(),
-#     legend.position = 'bottom',legend.justification='center',
-#     legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
-# )
-#
-# dev.off()
+pdf(paste0(file.loc.git,country,'_correlations_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_all_transport_against_road_only_fast_contig.pdf'),paper='a4r',height=0,width=0)
+print(
+    ggplot(data=dat.road.compare,aes(x=model.2.mean.x,y=model.2.mean.y)) +
+    geom_point() +
+    geom_errorbar(aes(ymin=model.2.ll.y,ymax=model.2.ul.y),alpha=0.2) +
+    geom_errorbarh(aes(xmin=model.2.ll.x,xmax=model.2.ul.x),alpha=0.2) +
+    geom_abline() +
+    coord_equal() +
+    xlab('Temperature parameters from all transport runs') + ylab('Temperature parameters from road traffic only runs') +
+    theme_bw() + theme(text = element_text(size = 15),
+    panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
+    plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
+)
+dev.off()
+
+pdf(paste0(file.loc.git,country,'_correlations_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_all_transport_against_non-road_only_fast_contig.pdf'),paper='a4r',height=0,width=0)
+print(
+    ggplot(data=dat.nonroad.compare,aes(x=model.2.mean.x,y=model.2.mean.y)) +
+    geom_point() +
+    geom_errorbar(aes(ymin=model.2.ll.y,ymax=model.2.ul.y),alpha=0.2) +
+    geom_errorbarh(aes(xmin=model.2.ll.x,xmax=model.2.ul.x),alpha=0.2) +
+    geom_abline() +
+    coord_equal() +
+    xlab('Temperature parameters from all transport runs') + ylab('Temperature parameters from non-road traffic only runs') +
+    theme_bw() + theme(text = element_text(size = 15),
+    panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
+    plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
+)
+dev.off()
+
